b1fb0e1aa6a3a24ca6de2655b8a58175a49eaa9b
Christopher Klayaani <c.klayaani@a2z.media>
2026-01-20 12:23:14 +0200

    fix CORS allowed origins by removing spaces in production URLs

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'https:// service.a2z.media', 'http:// service.a2z.media'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://service.a2z.media', 'http://service.a2z.media'],
     credentials: true,
   });
 
